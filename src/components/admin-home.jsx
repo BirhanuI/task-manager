7a87a5479dde3da51e906177abe38bdc771a46ab
BirhanuI <birhanunegash9@gmail.com>
2023-10-26 09:40:59 -0700

    finished 'profile' page

--- src/components/admin-home.jsx
+++ src/components/admin-home.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Popover } from "react-tiny-popover";
+import { FaEdit, FaTrash, FaUserPlus } from "react-icons/fa";
 import { HiOutlineMenu } from "react-icons/hi";
 import {
   Chart as ChartJS,
@@ -11,49 +13,399 @@ import {
 } from "chart.js";
 import { Bar } from "react-chartjs-2";
 const Dashboard = () => {
-  const [toggle, setToggle] = useState(false);
+  return (
+    <div>
+      <DashboardNav />
+      <AdminHome />
+      {/* <Employee /> */}
+      {/* <Tasks /> */}
+      {/* <Notification /> */}
+    </div>
+  );
+};
+export const Notification = () => {
   const analaysis = [
-    { title: "Tasks completed per Month", id: 0 },
-    { title: "Tasks completed per employee", id: 1 },
-    { title: "Tasks completed per employee", id: 2 },
+    {
+      title: "You've been assigned a new task",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 0,
+    },
+    {
+      title: "Tasks completed per employee",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 1,
+    },
+    {
+      title: "Tasks completed per employee",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 2,
+    },
+    {
+      title: "You've been assigned a new task",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 3,
+    },
+    {
+      title: "Tasks completed per employee",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 4,
+    },
+    {
+      title: "Tasks completed per employee",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 5,
+    },
+    {
+      title: "You've been assigned a new task",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 6,
+    },
+    {
+      title: "Tasks completed per employee",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 7,
+    },
+    {
+      title: "Tasks completed per employee",
+      body: "Hi John, you've been assigned the task 'Update website design'. Please check the task details and start working on it. Let me know if you have any questions.",
+      id: 8,
+    },
   ];
   const [i, setI] = useState(0);
   const [close, setClose] = useState(true);
   return (
-    <div>
-      <DashboardNav />
-      <div className="bg-white w-full flex justify-center items-center flex-col lg:relative ">
-        {analaysis.map((data, index) => (
-          <div className={`w-11/12 lg:flex lg:grow lg:gap-20 ${index==0?"lg:mt-20":""}`} key={data.id}>
+    <div className="bg-white flex">
+      <div className="w-full lg:h-96 scrollable-div lg:mt-16 lg:overflow-y-scroll overflow-x-hidden">
+      <div className="scrollable-div-inside w-full flex justify-center items-center flex-col lg:relative ">
+
+      {analaysis.map((data, index) => (
+        <div
+          className={` w-11/12 lg:flex lg:grow lg:gap-20 ${
+            index == 0 ? "lg:mt-2" : ""
+          } `}
+          key={data.id}
+        >
+          <div
+            onClick={() => {
+              setI(index);
+            }}
+            className={`lg:grow lg:max-w-sm ${
+              i == analaysis[index].id
+              ? "text-primary-500 bg-slate-100"
+                : "hover:bg-primary-300 text-white"
+            } cursor-pointer   py-5 bg-primary-400 rounded-t-lg lg:rounded-lg text-center mt-2 duration-1000`}
+          >
+            {data.title}
+          </div>
+          <div className="overflow-hidden lg:grow flex justify-center ">
             <div
-              onClick={() => {
-                setI(index);
-              }}
-              className={`lg:grow lg:max-w-sm ${
+              className={`flex justify-center px-5 border duration-700 w-full lg:max-w-xl ${
                 i == analaysis[index].id
-                  ? "text-primary-500 bg-slate-100"
-                  : "hover:bg-primary-300 text-white"
-              } cursor-pointer   py-5 bg-primary-400 rounded-lg text-center mt-2 duration-1000`}
+                  ? "py-2 lg:fixed lg:top-52 lg:mt-10"
+                  : "-mt-56 sm:-mt-72 lg:hidden "
+              }`}
             >
-              {data.title}
-            </div>
-            <div className="overflow-hidden lg:grow flex justify-center ">
-              <div
-                className={`w-5/6 duration-700 max-w-xl ${
-                  i == analaysis[index].id ? "mt-2 lg:absolute lg:top-0 lg:mt-10" : "-mt-56 sm:-mt-72 lg:hidden"
-                }`}
-              >
-                <Charts />
+              <div className="lg:h-full max-w-md lg:max-w-xl">
+                <div className="text-center font-bold text-primary-400 my-5 text-2xl hidden lg:block">
+                  {data.title}
+                </div>
+                <div className="lg:mb-10 text-primary-500">{data.body}</div>
               </div>
             </div>
           </div>
-        ))}
+        </div>
+      ))}
+</div></div>
+    </div>
+  );
+};
+export const Employee = () => {
+  const data = [
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+  ];
+  const tableHeading = [
+    "No.",
+    "Name",
+    "Age",
+    "Address",
+    "Phone",
+    "Role",
+    "ID",
+    "",
+  ];
+  return (
+    <div>
+      <div className="bg-white overflow-x-auto md:flex justify-center flex-col items-center">
+        <div>
+          <table className="text-primary-500 mt-5">
+            <thead>
+              <tr className="bg-primary-400 text-white">
+                {tableHeading.map((heading) => (
+                  <td className="px-5 py-3">{heading}</td>
+                ))}
+              </tr>
+            </thead>
+            {data.map((data, index) => (
+              <tbody key={index}>
+                <tr className={`${index % 2 == 0 ? "" : "bg-gray-100"}`}>
+                  <td className="px-5 py-2">{index + 1}</td>
+                  <td className="px-5">{data.name}</td>
+                  <td className="px-5">{data.age}</td>
+                  <td className="px-5">{data.address}</td>
+                  <td className="px-5">{data.phone}</td>
+                  <td className="px-5">{data.role}</td>
+                  <td className="px-5">{data.id}</td>
+                  <td className="flex gap-5 text-lg py-3">
+                    <FaEdit className="cursor-pointer" />
+                    <FaTrash className="text-red-500 cursor-pointer mr-2" />
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+          <div className="flex  mt-10 justify-between">
+            <FaUserPlus
+              className="text-primary-500 text-4xl"
+              title="Add Employee"
+            />
+            <div className="flex items-start justify-around w-2/6">
+              <div>
+                Rows per page{" "}
+                <select name="No. of rows" id="">
+                  <option value="10">10</option>
+                  <option value="10">15</option>
+                  <option value="10">20</option>
+                </select>
+              </div>
+              <div>{`${"1-8"} of ${data.length}`}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
-
-const DashboardNav = () => {
+export const Tasks = () => {
+  const data = [
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "1",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "2",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "3",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "4",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "5",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "6",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "7",
+    },
+    {
+      name: "berhanu",
+      age: "23",
+      address: "addis ababa",
+      phone: "0912874356",
+      role: "Frontend developer",
+      id: "8",
+    },
+  ];
+  const tableHeading = [
+    "No.",
+    "Name",
+    "Age",
+    "Address",
+    "Phone",
+    "Role",
+    "ID",
+    "",
+  ];
+  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
+  const [counter, setCounter] = useState();
+  return (
+    <div>
+      <div className="bg-white md:flex justify-center flex-col items-center">
+        <div className="overflow-x-auto   ">
+          <table className="text-primary-500 mt-5">
+            <thead>
+              <tr className="bg-primary-400 text-white">
+                {tableHeading.map((heading) => (
+                  <td className="px-5 py-3" key={heading}>
+                    {heading}
+                  </td>
+                ))}
+              </tr>
+            </thead>
+            {data.map((data, index) => (
+              <tbody key={index}>
+                <tr className={`${index % 2 == 0 ? "" : "bg-gray-100"}`}>
+                  <td className="px-5 py-2">{index + 1}</td>
+                  <td className="px-5">{data.name}</td>
+                  <td className="px-5">{data.age}</td>
+                  <td className="px-5 ">
+                    <Popover
+                      className="hidden"
+                      isOpen={isPopoverOpen}
+                      positions={["bottom", "left", "right"]}
+                      content={
+                        <div
+                          className={`${
+                            counter == data.id ? "" : "hidden"
+                          } bg-white p-5 text-primary-500 border border-primary-500 rounded-xl`}
+                        >
+                          Hi! I'm popover content.
+                        </div>
+                      }
+                    >
+                      <div
+                        onClick={() => {
+                          setIsPopoverOpen(!isPopoverOpen);
+                          setCounter(index + 1);
+                        }}
+                      >
+                        {data.address}
+                      </div>
+                    </Popover>
+                  </td>
+                  <td className="px-5">{data.phone}</td>
+                  <td className="px-5">{data.role}</td>
+                  <td className="px-5">{data.id}</td>
+                  <td className="flex gap-5 text-lg py-3">
+                    <FaEdit className="cursor-pointer" />
+                    <FaTrash className="text-red-500 cursor-pointer mr-2" />
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+        <div className="flex md:w-2/4 justify-between my-10 px-5 ">
+          <FaUserPlus
+            className="text-primary-500 text-4xl"
+            title="Add Employee"
+          />
+          <div className="flex items-start  md:justify-between ">
+            <div className="mr-5">
+              Rows per page{" "}
+              <select name="No. of rows" id="">
+                <option value="10">10</option>
+                <option value="10">15</option>
+                <option value="10">20</option>
+              </select>
+            </div>
+            <div className="">{`${"1-8"} of ${data.length}`}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export const DashboardNav = () => {
   const navLink = [
     { link: "home", title: "Home" },
     { link: "employee", title: "Employee" },
@@ -61,7 +413,7 @@ const DashboardNav = () => {
     { link: "notification", title: "Notification" },
   ];
   return (
-    <div>
+    <div className="border-b md:border-none">
       <div className="flex justify-between px-5 py-2 items-center text-primary bg-white underline md:hidden text-primary-500">
         <HiOutlineMenu className="text-4xl cursor-pointer" />
         <p>admin/home</p>
@@ -123,5 +475,51 @@ export const Charts = () => {
   };
 
   return <Bar options={options} data={data} />;
+};
+export const AdminHome = () => {
+  const [toggle, setToggle] = useState(false);
+  const analaysis = [
+    { title: "Tasks completed per Month", id: 0 },
+    { title: "Tasks completed per employee", id: 1 },
+    { title: "Tasks completed per employee", id: 2 },
+  ];
+  const [i, setI] = useState(0);
+  const [close, setClose] = useState(true);
+  return (
+    <div className="bg-white w-full flex justify-center items-center flex-col lg:relative ">
+      {analaysis.map((data, index) => (
+        <div
+          className={`w-11/12 lg:flex lg:grow lg:gap-20 ${
+            index == 0 ? "lg:mt-20" : ""
+          } `}
+          key={data.id}
+        >
+          <div
+            onClick={() => {
+              setI(index);
+            }}
+            className={`lg:grow lg:max-w-sm ${
+              i == analaysis[index].id
+                ? "text-primary-500 bg-slate-100"
+                : "hover:bg-primary-300 text-white"
+            } cursor-pointer   py-5 bg-primary-400 rounded-lg text-center mt-2 duration-1000`}
+          >
+            {data.title}
+          </div>
+          <div className="overflow-hidden lg:grow flex justify-center ">
+            <div
+              className={`w-5/6 duration-700 max-w-xl ${
+                i == analaysis[index].id
+                  ? "mt-2 lg:absolute lg:top-0 lg:mt-10"
+                  : "-mt-56 sm:-mt-72 lg:hidden"
+              }`}
+            >
+              <Charts />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default Dashboard;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,54 @@
 import Logo from "./../assets/Logo.svg";
-import {FaRegUserCircle} from 'react-icons/fa'
+import { FaRegUserCircle } from "react-icons/fa";
+import { FaGear, FaPrint, FaChartLine } from "react-icons/fa6";
+import { MdOutlineLogout } from "react-icons/md";
 const Navbar = () => {
   return (
     <div className="">
-      <div className="flex justify-around py-2 bg-gray-100">
+      <div className="flex justify-around py-2 bg-gray-100 relative z-20 sm:z-0">
         <img className="h-10 md:h-16" src={Logo} alt="logo" />
-        <div className="text-4xl text-orange-400 md:text-6xl cursor-pointer"><FaRegUserCircle /></div>
+        <div className="text-4xl text-orange-400 md:text-6xl cursor-pointer">
+          <FaRegUserCircle />
+        </div>
+      </div>
+      <div className="fixed top-0 bottom-0 left-0 right-0 backdrop-blur-md sm:z-10"></div>
+      <div className="w-full sm:flex ">
+        <Profile />
+      </div>
+    </div>
+  );
+};
+export const Profile = () => {
+  return (
+    <div className="relative sm:static">
+      <div className="absolute top-0 z-10 sm:w-96 sm:h-screen left-0 right-0 bg-primary-400 text-gray-100 rounded-bl-2xl rounded-br-2xl sm:rounded">
+        <div className="flex items-center ml-10 gap-5 border-b p-5">
+          <div className="text-7xl">
+            <FaRegUserCircle />
+          </div>
+          <div className="flex flex-col font-medium ">
+            <span className="text-xl">Berhanu Negash</span>
+            <span className="font-light">birhanunegash9@gmail.com</span>
+          </div>
+        </div>
+        <div className="flex justify-center items-center flex-col gap-5 ">
+          <div className="flex w-5/12 gap-5 mt-10 sm:mt-20 hover:text-gray-200 cursor-pointer">
+            <FaGear className="text-3xl" />
+            <div className="text-xl">Settings</div>
+          </div>
+          <div className="flex w-5/12 gap-5 hover:text-gray-200 cursor-pointer">
+            <FaChartLine className="text-3xl" />
+            <div className="text-xl">Activity Log</div>
+          </div>
+          <div className="flex w-5/12 gap-5 mb-16 hover:text-gray-200 cursor-pointer">
+            <FaPrint className="text-3xl" />
+            <div className="text-xl">Print</div>
+          </div>
+          <div className="flex  mb-10 w-5/12 gap-5 bottom hover:text-gray-200 cursor-pointer">
+            <MdOutlineLogout className="text-3xl" />
+            <div className="text-xl">Log Out</div>
+          </div>
+        </div>
       </div>
     </div>
   );
